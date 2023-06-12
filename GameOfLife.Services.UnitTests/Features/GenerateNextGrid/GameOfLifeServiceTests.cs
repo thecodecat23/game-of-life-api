d123d2fa41556722ffde90aa694c197ae48bdf3f
TheCodeCat <thecodecat23@gmail.com>
2023-06-12 17:34:22 +0200

    clean code

--- GameOfLife.Services.UnitTests/Features/GenerateNextGrid/GameOfLifeServiceTests.cs
+++ GameOfLife.Services.UnitTests/Features/GenerateNextGrid/GameOfLifeServiceTests.cs
@@ -1,9 +1,4 @@
 ﻿using GameOfLife.Services.Features.GenerateNextGrid;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameOfLife.API.UnitTests.Features.GenerateNextGrid;
 
